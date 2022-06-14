98342e01e2687e1dc1e6456c154e1efd58ec7e49
sdf0106 <d.sagatov@student.inha.uz>
2022-06-14 17:13:08 +0500

    feat(weather): done

--- lib/features/main/prensentation/pages/main_page.dart
+++ lib/features/main/prensentation/pages/main_page.dart
@@ -7,6 +7,7 @@ import '../../../counter /presentation/cubit/counter_cubit.dart';
 import '../../../counter /presentation/widgets/counter_text.dart';
 import '../../../weather/data/remote_data_source/data_source.dart';
 import '../../../weather/data/repositories/weather_repository_impl.dart';
+import '../../../weather/presentation/widgets/weather_info.dart';
 import '../widgets/buttons_group.dart';
 
 class MainPage extends StatelessWidget {
@@ -35,45 +36,15 @@ class MainPage extends StatelessWidget {
         child: Stack(
           children: [
             Center(
-                child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                BlocBuilder<WeatherBloc, WeatherState>(
-                  builder: (context, state) {
-                    return state.maybeWhen(
-                      loading: () {
-                        return const Center(
-                          child: CircularProgressIndicator.adaptive(),
-                        );
-                      },
-                      weatherGotten: (weather) {
-                        return Text(
-                          '${weather.current.tempC} - celcium',
-                          style: const TextStyle(fontSize: 20),
-                        );
-                      },
-                      failure: (errorMessage) {
-                        return Center(
-                          child: Text(
-                            errorMessage,
-                            style: const TextStyle(
-                              color: Colors.red,
-                            ),
-                          ),
-                        );
-                      },
-                      orElse: () {
-                        return const Text(
-                          'Load Weather Info',
-                        );
-                      },
-                    );
-                  },
-                ),
-                const SizedBox(height: 20.0),
-                const CounterText(),
-              ],
-            )),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  WeatherInfo(),
+                  SizedBox(height: 20.0),
+                  CounterText(),
+                ],
+              ),
+            ),
             const ButtonsGroup(),
           ],
         ),
---
+++ lib/features/weather/presentation/widgets/weather_info.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../bloc/weather/weather_bloc.dart';
+
+class WeatherInfo extends StatelessWidget {
+  const WeatherInfo({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<WeatherBloc, WeatherState>(
+      builder: (context, state) {
+        return state.maybeWhen(
+          loading: () {
+            return const Center(
+              child: CircularProgressIndicator.adaptive(),
+            );
+          },
+          weatherGotten: (weather) {
+            return Text(
+              '${weather.current.tempC} - celcium',
+              style: const TextStyle(fontSize: 20),
+            );
+          },
+          failure: (errorMessage) {
+            return Center(
+              child: Text(
+                errorMessage,
+                style: const TextStyle(
+                  color: Colors.red,
+                ),
+              ),
+            );
+          },
+          orElse: () {
+            return const Text(
+              'Load Weather Info',
+            );
+          },
+        );
+      },
+    );
+  }
+}
